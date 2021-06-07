8a3ba49a810673164b91b38476218e5934051014
李振源 <9091396+li-zhen-yuan@user.noreply.gitee.com>
2021-06-07 22:02:54 +0800

    三子棋（人机交互）

---
+++ test_2021_6_6/game1/game1/test.c
@@ -0,0 +1,81 @@
+#include"game.h"
+
+void menu()
+{
+	printf("*******************************\n");
+	printf("****** 1.play     0.exit ******\n");
+	printf("*******************************\n");
+}
+//游戏的整个实现
+void game()
+{
+	char ret = 0;
+	//数组-存放走出的棋盘信息
+	char board[ROW][COL] = {0};//下一步数组里放的数据需要先初始化，得到一个空棋盘。
+	//初始化棋盘
+	InitBoard(board, ROW, COL);
+	//打印棋盘
+	DisplayBoard(board, ROW, COL);
+	//下棋
+	while (1)
+	{
+		//玩家下棋
+		PlayerMove(board, ROW, COL);
+		DisplayBoard(board, ROW, COL);
+		//判断输赢
+		ret=IsWin(board, ROW, COL);
+		if (ret != 'C')
+		{
+			break;
+		}
+		//电脑下棋
+		ComputerMove(board, ROW, COL);
+		DisplayBoard(board, ROW, COL);
+		//判断输赢
+		ret = IsWin(board, ROW, COL);
+		if (ret != 'C')
+		{
+			break;
+		}
+	}
+	if (ret == '*')
+	{
+		printf("玩家赢\n");
+	}
+	else if (ret == '#')
+	{
+		printf("电脑赢\n");
+	}
+	else
+	{
+		printf("平局\n");
+	}
+}
+void test()
+{
+	srand((unsigned int)time(NULL));
+	int input = 0;
+	do
+	{
+		menu();
+		printf("请选择:");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			game();
+			break;
+		case 0:
+			printf("退出游戏\n");
+			break;
+		default:
+			printf("选择错误，请重新选择！\n");
+			break;
+		}
+	} while (input);
+}
+int main()
+{
+	test();
+	return 0;
+}
